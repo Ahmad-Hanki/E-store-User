f2fdf1d07a75b36c1b8012c7686a7f1aea3509f5
ahmad <itxti909@gmail.com>
2024-03-27 16:59:46 +0300

    finishes the cart page

--- src/components/ui/product-card.tsx
+++ src/components/ui/product-card.tsx
@@ -5,19 +5,19 @@ import { MouseEventHandler } from "react";
 import { Expand, ShoppingCart } from "lucide-react";
 import { useRouter } from "next/navigation";
 
-import Currency from "./Currency"; 
+import Currency from "./Currency";
 import IconButton from "./IconButton";
 import usePreviewModal from "@/hooks/use-preview-modal";
-import { Product } from "../../../types"; 
+import { Product } from "../../../types";
+import useCart from "@/hooks/use-cart";
 
 interface ProductCard {
-  data: Product
+  data: Product;
 }
 
-const ProductCard: React.FC<ProductCard> = ({
-  data
-}) => {
+const ProductCard: React.FC<ProductCard> = ({ data }) => {
   const previewModal = usePreviewModal();
+  const cart = useCart();
   const router = useRouter();
 
   const handleClick = () => {
@@ -29,29 +29,37 @@ const ProductCard: React.FC<ProductCard> = ({
 
     previewModal.onOpen(data);
   };
+  const onAddToCart: MouseEventHandler<HTMLButtonElement> = (event) => {
+    event.stopPropagation();
 
+    cart.addItem(data);
+  };
 
   
-  return ( 
-    <div onClick={handleClick} className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4">
+
+  return (
+    <div
+      onClick={handleClick}
+      className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4"
+    >
       {/* Image & actions */}
       <div className="aspect-square rounded-xl bg-gray-100 relative">
-        <Image 
-          src={data.images?.[0]?.url} 
-          alt="" 
+        <Image
+          src={data.images?.[0]?.url}
+          alt=""
           fill
           className="aspect-square object-cover rounded-md"
         />
         <div className="opacity-0 group-hover:opacity-100 transition absolute w-full px-6 bottom-5">
           <div className="flex gap-x-6 justify-center">
-            <IconButton 
+            <IconButton
               onClick={onPreview}
-              className="z-40" 
+              className="z-40"
               icon={<Expand size={20} className="text-gray-600" />}
             />
             <IconButton
-              onClick={() => {}} 
-              icon={<ShoppingCart size={20} className="text-gray-600" />} 
+              onClick={onAddToCart}
+              icon={<ShoppingCart size={20} className="text-gray-600" />}
             />
           </div>
         </div>
@@ -67,6 +75,6 @@ const ProductCard: React.FC<ProductCard> = ({
       </div>
     </div>
   );
-}
+};
 
 export default ProductCard;
